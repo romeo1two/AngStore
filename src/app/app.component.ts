7031c4cf93ca582d1be951a7c3c7f22ec88eea7a
romeo1two <romeo1two@gmail.com>
2018-10-23 12:10:40 +0300

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: `<div class="bg-success p-2 text-center text-white">
+                    This is SportsStore
+               </div>`
+})
+export class AppComponent {
+  title = 'app';
+}
