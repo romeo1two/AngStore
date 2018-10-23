302b682203c0372e2bbe90925e20d4963cfda4cf
romeo1two <romeo1two@gmail.com>
2018-10-23 13:29:10 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app',
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
-  template: `<div class="bg-success p-2 text-center text-white">
-                    This is SportsStore
-               </div>`
+  // template: `<div class="bg-success p-2 text-center text-white">
+  //                   This is SportsStore
+  //              </div>`
+  template: "<store></store>"
 })
 export class AppComponent {
   title = 'app';
