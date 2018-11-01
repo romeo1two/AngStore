2f1a4eb61fc74d3d8212de7b7c70cb45f28968f7
romeo1two <romeo1two@gmail.com>
2018-11-01 15:03:40 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,15 +16,23 @@ import { StoreFirstGuard } from "./storeFirst.guard";
     BrowserModule,
     StoreModule,
     RouterModule.forRoot([
-      {path: "store", component: StoreComponent,
-      canActivate: [StoreFirstGuard]
-    },
-      {path: "cart", component: CartDetailComponent,
-      canActivate: [StoreFirstGuard]
-    },
-      {path: "checkout", component: CheckoutComponent,
-      canActivate: [StoreFirstGuard]
-    },
+      {
+        path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "admin",
+        loadChildren: "./admin/admin.module#AdminModule",
+        canActivate: [StoreFirstGuard]
+      },
       {path: "**", redirectTo: "/store"}
     ])
   ],
